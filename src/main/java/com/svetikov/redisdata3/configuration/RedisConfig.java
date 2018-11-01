d077afd0f418a8b0a34a127ea61a748cca46623b
Sveticov <sveticovdanil@gmail.com>
2018-11-01 10:59:17 +0300

    LettuceConnectionFactory SpringBoot 2.1

--- src/main/java/com/svetikov/redisdata3/configuration/RedisConfig.java
+++ src/main/java/com/svetikov/redisdata3/configuration/RedisConfig.java
@@ -16,9 +16,9 @@ public class RedisConfig {
 
     @Bean
     LettuceConnectionFactory jedisConnectionFactory(){
-        return new LettuceConnectionFactory(new RedisStandaloneConfiguration("localhost",6379));
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration("192.168.56.101",7702));
     }
-
+//show picture for config redis ubuntu Linux
     @Bean
     public RedisTemplate<String,Object> redisTemplate(){
         final RedisTemplate<String,Object> template=new RedisTemplate<String, Object>();
